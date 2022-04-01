6bd8b9cd6dcd3d2dc4a4b1c6d4ec217c22957e5d
AdaevR <50821725+AdaevR@users.noreply.github.com>
2022-04-01 16:14:31 +0300

    data update

--- lab7.py
+++ lab7.py
@@ -1,21 +1,20 @@
 import math
+from turtle import right
 import matplotlib.pyplot as plt
 import json
 
-from sympy import true
-
 
 def main():
-    data = LoadData('data/data2_1.json')
+    data = LoadData('data/7_2/data2.json')
 
-    isLinear = true
+    isLinear = False
     if(isLinear):
         rxy = LinearCorrelationCoeff(data)
-        print("Значение коэффициента корреляции: " + str(rxy))
+        print("Значение коэффициента корреляции: " + str(round(rxy, 4)))
     else:
         rs = RangCorrelationCoeff(data)
-        print("Значение коэффициента корреляции: " + str(rs[0]))
-        print("Значение t_кр: " + str(rs[1]))
+        print("Значение коэффициента корреляции: " + str(round(rs[0], 4)))
+        print("Значение t_расч: " + str(round(rs[1], 4)))
 
     ShowCorrelationPlot(data)
 
@@ -49,16 +48,35 @@ def LinearCorrelationCoeff(data):
 
     return numerator/denominator
 
+def CalcRank(index, count):
+    left = (index+count)*((index+count)+1)/2
+    right = (index-1)*(index)/2
+    return (left-right)/(count+1)
+
+def GetRankedList(list):
+    seq = sorted(list)
+    return [CalcRank(seq.index(v)+1, list.count(v)-1) for v in list]
+
 def RangCorrelationCoeff(data):
+    x_data_ranks = GetRankedList(list(zip(*data))[0])
+    y_data_ranks = GetRankedList(list(zip(*data))[1])
+    print("x_data_ranks: " + str(x_data_ranks))
+    print("y_data_ranks: " + str(y_data_ranks))
+
+    di = []
     di2 = []
-
-    for item in data:
-        di = item[0] - item[1]
-        di2.append(di*di)
-
+    for i in range(len(data)):
+        di_item = x_data_ranks[i] - y_data_ranks[i]
+        di.append(di_item)
+        di2.append(di_item*di_item)
+    
+    print("di: " + str(di) + " Сумма " + str(sum(di)))
+    print("di^2: " + str(di2) + " Сумма " + str(sum(di2)))
+    
     n = len(data)
     rs = 1 - (6*sum(di2)) / (n * (n*n - 1))
     t_calc = abs(rs)*math.sqrt((n-2)/(1-rs*rs))
+
     return (rs, t_calc)
 
 def ShowCorrelationPlot(data):
